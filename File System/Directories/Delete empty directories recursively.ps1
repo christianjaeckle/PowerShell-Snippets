6fd109ec5ff374e8342fea997c8f9c20518722f9
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-25 22:37:46 +0200

    Update Delete empty directories recursively.ps1

--- File System/Directories/Delete empty directories recursively.ps1
+++ File System/Directories/Delete empty directories recursively.ps1
@@ -1,6 +1,6 @@
 $RootPath = 'C:\Windows'
 
-Get-ChildItem $RootPath -Directory -Recurse | ForEach-Object {
+Get-ChildItem -Path $RootPath -Directory -Recurse | ForEach-Object {
     if((Get-ChildItem $_.FullName -ErrorAction SilentlyContinue) -eq $null) {
         Remove-Item -Path $_.FullName -ErrorAction SilentlyContinue -Confirm:$true -WhatIf
     }
